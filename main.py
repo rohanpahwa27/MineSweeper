5d54221f84b5de56b484fbe5ee2552414512f708
beatrice <blianggilman@gmail.com>
2020-02-22 15:39:54 -0500

    object completed i think

--- main.py
+++ main.py
@@ -28,12 +28,59 @@ def createMine():
             mine_location.add(value)
             x = int(value%dim)
             y = int(value/dim)
-            random_matrix[x][y] = 1
+            random_matrix[x,y] = 9 #numerical representation of mine
 
-    print(random_matrix)
+    return random_matrix
 
-createMine()
+matrix = createMine()
+print(matrix)
+
+def isValid(i, j):
+    if(i>=0 and i<dim and j>=0 and j<dim):
+        return True
+    return False
+
+#count mines around a specific index
+def countMines(x, y, matrix):
+    #get all 8 coordinates around a specific x,y coordinate and increment if it is a mine
+    num_mines_around=0
+    if(isValid(x+1, y)):
+        if(matrix[x+1][y]==9):
+            num_mines_around+=1
+    if(isValid(x+1, y+1)):
+        if(matrix[x+1][y+1]==9):
+            num_mines_around+=1
+    if(isValid(x, y+1)):
+        if(matrix[x][y+1]==9):
+            num_mines_around+=1
+    if(isValid(x-1, y+1)):
+        if(matrix[x-1][y+1]==9):
+            num_mines_around+=1
+    if(isValid(x-1, y)):
+        if(matrix[x-1][y]==9):
+            num_mines_around+=1
+    if(isValid(x-1, y-1)):
+        if(matrix[x-1][y-1]==9):
+            num_mines_around+=1
+    if(isValid(x, y-1)):
+        if(matrix[x][y-1]==9):
+            num_mines_around+=1
+    if(isValid(x+1, y-1)):
+        if(matrix[x+1][y-1]==9):
+            num_mines_around+=1
+    return num_mines_around
+
+for i in range(0, dim):
+    for j in range(0, dim):
+        if(matrix[i,j]!=9):
+            mine_num = countMines(i,j,matrix)
+            matrix[i, j] = mine_num
+
+print("---------ANSWER-------KEY-----------------------")
+print(matrix)
+
 
 
 kb1 = KB(True,0,0,0,0)
 print(kb1)
+print(kb1.mine)
