7a319f5203e5b78571dce24bedb6c33c6d6125a4
nishajagadeesh <nishaj474@gmail.com>
2020-02-27 20:59:25 -0500

    more colors! and stuff and comments

---
+++ graphics.py
@@ -0,0 +1,67 @@
+import pygame
+
+#DISPLAY GRAPHICS
+def display_graphics(matrix, dim):
+    pygame.init()
+
+    #set up drawing window
+    screen = pygame.display.set_mode([520,520])
+
+    pygame.display.set_caption("Play Board")
+    running = True
+
+    font = pygame.font.SysFont("arial", 36)
+
+    #run the game
+    while running:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running = False
+
+        screen.fill((255,255,255)) #fill background with white
+
+        #define variables
+        width = int(500/dim)
+        black = (0,0,0)
+        orange = (255,69,0)
+        green = (0,100,0)
+        blue = (0,0,128)
+
+        #draw grid
+        pygame.draw.rect(screen, black, (10,10,500,500),2)
+        for i in range(int(500/dim), 500, int(500/dim)):
+            pygame.draw.line(screen, black,(i+10,10),(i+10,510))
+            pygame.draw.line(screen, black,(10,i+10),(510,i+10))
+
+
+        #draw mines
+        radius = int(width/3) #radius of mine
+        for i in range(0, dim):
+            for j in range(0, dim):
+                if(matrix[i,j].mine == 1):
+                    si = 10+(i*width)
+                    sj = 10+(j*width)
+                    pygame.draw.circle(screen, orange, (int((10+(i*width)+(width/2))),int((10+(j*width)+(width/2)))),radius)
+                    pygame.draw.polygon(screen, orange, ((si+width/2,sj+(width/10)),(si+(width/3),sj+(width/4)),(si+(2*width/3),sj+(width/4)))) #top triangle
+                    pygame.draw.polygon(screen, orange, ((si+width/2,sj+(9*width/10)),(si+(width/3),sj+(3*width/4)),(si+(2*width/3),sj+(3*width/4)))) #bottom triangle
+                    pygame.draw.polygon(screen, orange, ((si+(2*width/3),sj+(width/4)),(si+(5*width/6),sj+(width/4)),(si+(3*width/4),sj+(width/2)))) #top right
+                    pygame.draw.polygon(screen, orange, ((si+(3*width/4),sj+(width/2)),(si+(5*width/6),sj+(3*width/4)),(si+(2*width/3),sj+(3*width/4)))) #bottom right
+                    pygame.draw.polygon(screen, orange, ((si+(width/3),sj+(width/4)),(si+(width/6),sj+(width/4)),(si+(width/4),sj+(width/2)))) #top left
+                    pygame.draw.polygon(screen, orange, ((si+(width/4),sj+(width/2)),(si+(width/6),sj+(3*width/4)),(si+(width/3),sj+(3*width/4)))) #bottom left
+
+                if(matrix[i,j].numMines >= 0 and matrix[i,j].numMines <= 8):
+                    color = green
+                if(matrix[i,j].numMines == 0):
+                    color = black
+                if(matrix[i,j].numMines == 1):
+                    color = green
+                if(matrix[i,j].numMines == 2):
+                    color = blue
+                #add colors for rest of numbers
+                if(matrix[i,j].mine == 2):
+                    #draw numbers in box
+                    text = font.render(str(matrix[i][j].numMines), True, color)
+                    screen.blit(text,(i*width+10+(width/3),j*width+10+(width/8)))
+        pygame.display.flip()
+
+    pygame.quit()
--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 import numpy as np
 from dataclasses import dataclass
-import pygame
 import random
+import pygame
 from pprint import pprint
-
+import graphics
+import graphicsAk
 
 @dataclass
 class KB():
@@ -85,141 +86,121 @@ kb1 = KB(0,0,0,0,0)
 print(kb1)
 print(kb1.mine)
 
-#START game
-# MAKE AN ARRAY OF EMPTY KBS THAT THE AI USES
-playboard = np.full((dim, dim), KB(0,0,0,0,0))
-#pprint (playboard)
-
-# go through the board and update KB:
-minesfound=0
-
-matrix_cpy = matrix
-print (matrix)
-for v in range(0, dim):
-    for w in range(0, dim):
-        print (matrix[v,w])
-        if matrix[v,w]==9:
-            minesfound= minesfound+1
-            playboard[v,w].mine = 1
-        else:
-            playboard[v,w].numMines = matrix[v,w]
-            playboard[v,w].mine = 2
-        print(playboard[v,w])
-print (playboard)
-
-
-#DISPLAY GRAPHICS
-pygame.init()
-
-#set up drawing window
-screen = pygame.display.set_mode([520,520])
-running = True
-
-font = pygame.font.SysFont("arial", 36)
-
-#run the game
-while running:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            running = False
-
-    screen.fill((255,255,255)) #fill background with white
-
-    #define variables
-    width = int(500/dim)
-    black = (0,0,0)
-    orange = (255,69,0)
-    green = (0,100,0)
-    blue = (0,0,128)
-
-    #draw grid
-    pygame.draw.rect(screen, black, (10,10,500,500),2)
-    for i in range(int(500/dim), 500, int(500/dim)):
-        pygame.draw.line(screen, black,(i+10,10),(i+10,510))
-        pygame.draw.line(screen, black,(10,i+10),(510,i+10))
-
-
-    #draw mines
-    radius = int(width/3) #radius of mine
-    for i in range(0, dim):
-        for j in range(0, dim):
-            if(matrix[i,j] == 9):
-                si = 10+(i*width)
-                sj = 10+(j*width)
-                pygame.draw.circle(screen, orange, (int((10+(i*width)+(width/2))),int((10+(j*width)+(width/2)))),radius)
-                pygame.draw.polygon(screen, orange, ((si+width/2,sj+(width/10)),(si+(width/3),sj+(width/4)),(si+(2*width/3),sj+(width/4)))) #top triangle
-                pygame.draw.polygon(screen, orange, ((si+width/2,sj+(9*width/10)),(si+(width/3),sj+(3*width/4)),(si+(2*width/3),sj+(3*width/4)))) #bottom triangle
-                pygame.draw.polygon(screen, orange, ((si+(2*width/3),sj+(width/4)),(si+(5*width/6),sj+(width/4)),(si+(3*width/4),sj+(width/2)))) #top right
-                pygame.draw.polygon(screen, orange, ((si+(3*width/4),sj+(width/2)),(si+(5*width/6),sj+(3*width/4)),(si+(2*width/3),sj+(3*width/4)))) #bottom right
-                pygame.draw.polygon(screen, orange, ((si+(width/3),sj+(width/4)),(si+(width/6),sj+(width/4)),(si+(width/4),sj+(width/2)))) #top left
-                pygame.draw.polygon(screen, orange, ((si+(width/4),sj+(width/2)),(si+(width/6),sj+(3*width/4)),(si+(width/3),sj+(3*width/4)))) #bottom left
-
-            if(matrix[i,j] >= 0 and matrix[i,j] <= 8):
-                color = green
-            if(matrix[i,j] == 0):
-                color = black
-            if(matrix[i,j] == 1):
-                color = green
-            if(matrix[i,j] == 2):
-                color = blue
-            #add colors for rest of numbers
-            if(matrix[i,j] >= 0 and matrix[i,j] <= 8):
-                #draw numbers in box
-                text = font.render(str(matrix[i][j]), True, color)
-                screen.blit(text,(i*width+10+(width/3),j*width+10+(width/8)))
-    pygame.display.flip()
-
-
-
-
-#START game
+# START game
 # MAKE AN ARRAY OF EMPTY KBS THAT THE AI USES
 
-playboard = np.empty(shape=(dim,dim), dtype=object)
-for o in range(0, dim):
-    for p in range(0, dim):
-        KBTemp= KB(0,0,0,0,0)
-        playboard[o][p]= KBTemp
-#print (playboard)
+# playboard = np.empty(shape=(dim,dim), dtype=object)
+# for o in range(0, dim):
+#     for p in range(0, dim):
+#         KBTemp= KB(0,0,0,0,0)
+#         playboard[o][p]= KBTemp
+# #print (playboard)
+#
+#
+#
+# # method to count how many identified mines
+# '''
+# def updateBlock(x, y, playboard):
+#     #check all 8 spots around
+#     #edge cases
+#     if
+# '''
+#
+# # go through the board and update KB:
+# minesfound=0
+#
+# pygame.init()
+#
+# # initilize set of random numbers
+# randomspots= set()
+# v = random.randint(0,dim-1)
+# w = random.randint(0,dim-1)
+#
+# #set up drawing window
+# screen = pygame.display.set_mode([520,520])
+#
+# pygame.display.set_caption("Play Board")
+# running = True
+#
+# font = pygame.font.SysFont("arial", 36)
+#
+# #run the game
+# while running:
+#     for event in pygame.event.get():
+#         if event.type==pygame.QUIT:
+#             running = False
+#
+#     screen.fill((255,255,255)) #fill background with white
+#
+#     #define variables
+#     width = int(500/dim)
+#     black = (0,0,0)
+#     orange = (255,69,0)
+#     green = (0,100,0)
+#     blue = (0,0,128)
+#
+#     #draw grid
+#     pygame.draw.rect(screen, black, (10,10,500,500),2)
+#     for i in range(int(500/dim), 500, int(500/dim)):
+#         pygame.draw.line(screen, black,(i+10,10),(i+10,510))
+#         pygame.draw.line(screen, black,(10,i+10),(510,i+10))
+#
+#     #pygame.display.flip()
+#
+#     #draw mines
+#     radius = int(width/3) #radius of mine
+#     for i in range(0, dim):
+#         for j in range(0, dim):
+#             #for l in range(0, dim*dim):
+#             # while (dim*v + w) in randomspots:
+#             #     v = random.randint(0,dim-1)
+#             #     w = random.randint(0,dim-1)
+#             # randomspots.add(dim*v+w)
+#             # #print (v,w)
+#             # if matrix[v,w]==9:
+#             #
+#             #     minesfound= minesfound+1
+#             #     playboard[v,w].mine = 1
+#             #     playboard[v,w].numMines = 9
+#             # else:
+#             #     playboard[v,w].numMines = matrix[v,w]
+#             #     playboard[v,w].mine = 2
+#
+#             #pygame.time.wait(500)
+#             if(playboard[i,j].mine == 1):
+#                 si = 10+(i*width)
+#                 sj = 10+(j*width)
+#                 pygame.draw.circle(screen, orange, (int((10+(i*width)+(width/2))),int((10+(j*width)+(width/2)))),radius)
+#                 pygame.draw.polygon(screen, orange, ((si+width/2,sj+(width/10)),(si+(width/3),sj+(width/4)),(si+(2*width/3),sj+(width/4)))) #top triangle
+#                 pygame.draw.polygon(screen, orange, ((si+width/2,sj+(9*width/10)),(si+(width/3),sj+(3*width/4)),(si+(2*width/3),sj+(3*width/4)))) #bottom triangle
+#                 pygame.draw.polygon(screen, orange, ((si+(2*width/3),sj+(width/4)),(si+(5*width/6),sj+(width/4)),(si+(3*width/4),sj+(width/2)))) #top right
+#                 pygame.draw.polygon(screen, orange, ((si+(3*width/4),sj+(width/2)),(si+(5*width/6),sj+(3*width/4)),(si+(2*width/3),sj+(3*width/4)))) #bottom right
+#                 pygame.draw.polygon(screen, orange, ((si+(width/3),sj+(width/4)),(si+(width/6),sj+(width/4)),(si+(width/4),sj+(width/2)))) #top left
+#                 pygame.draw.polygon(screen, orange, ((si+(width/4),sj+(width/2)),(si+(width/6),sj+(3*width/4)),(si+(width/3),sj+(3*width/4)))) #bottom left
+#
+#
+#             if(playboard[i,j].numMines >= 0 and playboard[i,j].numMines <= 8):
+#                 color = green
+#             if(playboard[i,j].numMines == 0):
+#                 color = black
+#             if(playboard[i,j].numMines == 1):
+#                 color = green
+#             if(playboard[i,j].numMines == 2):
+#                 color = blue
+#             #add colors for rest of numbers
+#             if(playboard[i,j].mine == 2):
+#                 #draw numbers in box
+#                 text = font.render(str(playboard[i][j].numMines), True, color)
+#                 screen.blit(text,(i*width+10+(width/3),j*width+10+(width/8)))
+#
+#             #pygame.display.update(pygame.Rect(i*width+10, j*width+10, width, width))
+#     pygame.display.flip()
+#
+#
+#
+# pygame.quit()
 
-# initilize set of random numbers
-randomspots= set()
-v = random.randint(0,9)
-w = random.randint(0,9)
-
-# method to count how many identified mines
-'''
-def updateBlock(x, y, playboard):
-    #check all 8 spots around
-    #edge cases
-    if
-'''
 
 
-
-
-# go through the board and update KB:
-minesfound=0
-matrix_cpy = matrix
-for l in range(0, dim):
-    for m in range(0, dim):
-        while (dim*v + w) in randomspots:
-            v = random.randint(0,9)
-            w = random.randint(0,9)
-        randomspots.add(dim*v+w)
-        #print (v,w)
-        if matrix[v,w]==9:
-
-            minesfound= minesfound+1
-            playboard[v,w].mine = 1
-            playboard[v,w].numMines = 9
-        else:
-            playboard[v,w].numMines = matrix[v,w]
-            playboard[v,w].mine = 2
-        #print(playboard[v,w])
-print (playboard)
-
-
-
-
-pygame.quit()
+graphicsAk.display_graphics(matrix, dim)
+#graphics.display_graphics(playboard, dim)
