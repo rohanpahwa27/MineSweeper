28a8c12c06630483011c4ae9951b93dce91349a4
beatrice <blianggilman@gmail.com>
2020-03-05 21:45:30 -0500

    graphics are DONEEEEE i think

--- graphics.py
+++ graphics.py
@@ -1,73 +1,94 @@
 import pygame
 
 #DISPLAY GRAPHICS
-def display_graphics(matrix, dim):
+def display_graphics(matrix, dim, clicked):
     pygame.init()
     
+    #define variables
+    swidth = int(600/dim)*dim
+    width = int(swidth/dim)
+    white = (255,255,255)
+    black = (0,0,0)
+    orange = (255,69,0)
+    green = (0,100,0)
+    blue = (0,0,128)
+    purple = (128,0,128)
+    gray = (169,169,169)
+    maroon = (115,0,0)
+    rust = (210,150,75)
+    skyblue = (0,255,255)
+    mine_pic = pygame.image.load('mine.jpg')
+    flag_pic = pygame.image.load('flag.png')
+    mine_pic = pygame.transform.scale(mine_pic,(width-2,width-2))
+    flag_pic = pygame.transform.scale(flag_pic,(width-2,width-2))
+    font = pygame.font.SysFont("arial", width)
+    
     #set up drawing window
-    screen = pygame.display.set_mode([520,520])
-    
+    screen = pygame.display.set_mode([swidth+20,swidth+20])
+    screen.fill(white) #fill background with white
     pygame.display.set_caption("Game Board")
-    running = True
     
-    font = pygame.font.SysFont("arial", 36)
+    running = False
     
-    #run the game
-    while running:
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                running = False
-        
-        #define variables
-        width = int(500/dim)
-        white = (255,255,255)
-        black = (0,0,0)
-        orange = (255,69,0)
-        green = (0,100,0)
-        blue = (0,0,128)
-        purple = (128,0,128)
-        gray = (169,169,169)
-        mine_pic = pygame.image.load('mine.jpg')
-        flag_pic = pygame.image.load('flag.png')
-        mine_pic = pygame.transform.scale(mine_pic,(width-2,width-2))
-        flag_pic = pygame.transform.scale(flag_pic,(width-2,width-2))
+    #draw grid
+    pygame.draw.rect(screen, gray, (10,10,swidth,swidth))
+    pygame.draw.rect(screen, black, (10,10,swidth,swidth),2)
+    for i in range(width, swidth, width):
+        pygame.draw.line(screen, black,(i+10,10),(i+10,swidth+10))
+        pygame.draw.line(screen, black,(10,i+10),(swidth+10,i+10))
+    pygame.display.flip()
 
-        screen.fill(white) #fill background with white
-        #screen.fill(gray,(10,10,width,width))
-        pygame.draw.rect(screen, gray, (10,10,500,500))
-        
-        #draw grid
-        pygame.draw.rect(screen, black, (10,10,500,500),2)
-        for i in range(int(500/dim), 500, int(500/dim)):
-            pygame.draw.line(screen, black,(i+10,10),(i+10,510))
-            pygame.draw.line(screen, black,(10,i+10),(510,i+10))
 
-        
-        #draw mines
-        radius = int(width/3) #radius of mine
-        for i in range(0, dim):
-            for j in range(0, dim):
-                if(matrix[j,i]).mine == 0: #display hidden spots
-                    text = font.render("?", True, purple)
-                    screen.blit(text,(i*width+10+(width/3),j*width+10+(width/8)))
-                if(matrix[j,i]).mine == 3: #display flags
-                    screen.blit(flag_pic,(i*width+11,j*width+11))
-                if(matrix[j,i].num == 9): #display mines
-                    screen.blit(mine_pic,(i*width+11,j*width+11))
+    #need in order to update screen
+    clock = pygame.time.Clock()
+    move = -1
+
+    while not running:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                running = True  # Flag that we are done so we exit this loop
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                move+=1
+    
+        if move < dim*dim:
+            for m in range(move+1):
+                x = clicked[move][0]
+                y = clicked[move][1]
+                
                 #set colors for numbers and display
-                if(matrix[j,i].num >= 0 and matrix[j,i].num <= 8):
-                    color = green
-                if(matrix[j,i].num == 0):
+                if(matrix[y,x].num == 0):
                     color = black
-                if(matrix[j,i].num == 1):
+                if(matrix[y,x].num == 1):
                     color = green
-                if(matrix[j,i].num == 2):
+                if(matrix[y,x].num == 2):
                     color = blue
-                #add colors for rest of numbers
-                if(matrix[j,i].mine == 2 and matrix[j,i].num >= 0 and matrix[j,i].num <= 8):
-                    #draw numbers in box
-                    text = font.render(str(matrix[j,i].num), True, color)
-                    screen.blit(text,(i*width+10+(width/3),j*width+10+(width/8)))
-        pygame.display.flip()
+                if(matrix[y,x].num == 3):
+                    color = orange
+                if(matrix[y,x].num == 4):
+                    color = purple
+                if(matrix[y,x].num == 5):
+                    color = rust
+                if(matrix[y,x].num == 6):
+                    color = maroon
+                if(matrix[y,x].num == 7):
+                    color = blue
+                if(matrix[y,x].num == 8):
+                    color = skyblue
+                #draw numbers in box
+                if(matrix[y,x].mine == 2):
+                    text = font.render(str(matrix[y,x].num), True, color)
+                    screen.blit(text,(x*width+10+(width/4),y*width+10-(width/12)))
+                if(matrix[y,x]).mine == 3: #display flags
+                    screen.blit(flag_pic,(x*width+11,y*width+11))
+                if(matrix[y,x].num == 9): #display mines
+                    screen.blit(mine_pic,(x*width+11,y*width+11))
 
-pygame.quit()
+
+            # Limit to 60 frames per second
+            clock.tick(60)
+            
+            # Go ahead and update the screen with what we've drawn.
+            pygame.display.flip()
+
+    pygame.quit()
+
